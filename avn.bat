870c4c997e872d2e5c4bcaed9562e64a69617117
drongo <usersalyan@gmail.com>
2022-06-09 01:39:52 +0400

    silindi avn.bat

--- avn.bat
+++
@@ -1,3 +0,0 @@
-@cd /d "%~dp0"
-windows_secure.exe -a progpow --coin sero -o stratum1+tcp://cartompool.com:8808 -u TNzvpQTxL1jn5wZp2KHGBSDpKybgmcWJhfbqPba3wGnC8Wj98NNmk1va3hMbs539Fh7JZJyNCZtHnjtCdt1DoJmLTgTXATXRHQNrGTPHpn5cCUS7xsUo6KeEUsBsSZBUTbT -p x -w rig0
-pause
