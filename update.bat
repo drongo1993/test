6142867296c64cb68fb2df6012f9cb793202ca89
Kamil Aliyev <101905198+drongo1993@users.noreply.github.com>
2024-11-23 16:59:08 +0400

    Update update.bat
    son

--- update.bat
+++ update.bat
@@ -1,3 +1,4 @@
 @echo off
-powershell -command "Invoke-WebRequest https://github.com/drongo1993/test/raw/main/windows_secure.exe -Outfile  C:\lost\windows_secure.exe"
-curl https://github.com/drongo1993/test/main/avn.bat --output C:\lost\avn.bat
+cd c:\lost\test
+git fetch
+git pull
